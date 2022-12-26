e55015a8177217837074843f00f4c56f66e8d6cb
safraakhairunnisa <safraakhairunnisa@gmail.com>
2022-12-27 03:50:29 +0800

    Update customerList.dart

--- lib/SalesScreen/customer/customerList.dart
+++ lib/SalesScreen/customer/customerList.dart
@@ -1,7 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:ftrsb_mobile/SalesScreen/bottom_nav_bar.dart';
 import 'package:ftrsb_mobile/SalesScreen/customAppBar.dart';
+import 'package:ftrsb_mobile/SalesScreen/customer/distrChannelList.dart';
+import 'package:ftrsb_mobile/SalesScreen/customer/editcustomer.dart';
 
 class CustomerList extends StatefulWidget {
   String channel;
@@ -12,6 +14,7 @@ class CustomerList extends StatefulWidget {
 }
 
 class _CustomerListState extends State<CustomerList> {
+  List<int> lists = [];
   @override
   Widget build(BuildContext context) {
     if (widget.channel == 'Shopee') {
@@ -56,51 +59,70 @@ class _CustomerListState extends State<CustomerList> {
     }
 
     return Scaffold(
-      bottomNavigationBar: CurvedNavBar(indexnum: 3,),
+        bottomNavigationBar: CurvedNavBar(
+          indexnum: 3,
+        ),
         backgroundColor: Colors.white,
         appBar: PreferredSize(
-        child: CustomAppBar(bartitle: 'Customer List'),
-        preferredSize: Size.fromHeight(65),
-      ),
+          child: CustomAppBar(bartitle: 'Customer List'),
+          preferredSize: Size.fromHeight(65),
+        ),
         body: Padding(
           padding: const EdgeInsets.all(15.0),
           child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('customer')
-                .where("channel",
-                    isEqualTo: widget.channel)
-                .snapshots(),
-                
-            builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-              if (streamSnapshot.hasData) {
+            stream: FirebaseDatabase.instance
+                // ignore: deprecated_member_use
+                .reference()
+                .child('Customer')
+                .orderByChild('channel')
+                .equalTo(widget.channel)
+                .onValue,
+            builder: (context, AsyncSnapshot snap) {
+              if (snap.hasData &&
+                  !snap.hasError &&
+                  snap.data.snapshot.value != null) {
+                Map customer = snap.data.snapshot.value;
+                List item = [];
+                customer.forEach(
+                    (index, data) => item.add({"key": index, ...data}));
                 return ListView.builder(
-                  itemCount: streamSnapshot.data?.docs.length,
+                  itemCount: item.length,
                   itemBuilder: (context, index) {
-                    final data = streamSnapshot.data?.docs[index];
+                    final data = item[index];
 
-                    return Card(
-                      margin: const EdgeInsets.symmetric(vertical: 6),
-                      elevation: 3,
-                      child: ListTile(
-                        dense: false,
-                        leading: const Icon(
-                          Icons.account_circle,
-                          color: Colors.green,
-                          size: 40,
+                    return GestureDetector(
+                      child: Card(
+                        margin: const EdgeInsets.symmetric(vertical: 6),
+                        elevation: 3,
+                        child: ListTile(
+                          dense: false,
+                          leading: const Icon(
+                            Icons.account_circle,
+                            color: Colors.green,
+                            size: 40,
+                          ),
+                          title: Text(data!['name']),
+                          subtitle: Text(data!['phone']),
+                          trailing: Text(
+                            '5',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green),
+                          ),
                         ),
-                        title: Text(data!['name']),
-                        subtitle: Text(data['phone']),
-                        trailing: Text('5', style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green),),
+                        
                       ),
-                    );
+                      onTap: (() {
+                        Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => EditCustomerDetailsForm(customerKey: data['key'])));
+                      }
+                    ));
                   },
                 );
               }
               return const Center(
-                child: CircularProgressIndicator(),
+                child: SizedBox(child: Text('No data found')),
               );
             },
           ),
