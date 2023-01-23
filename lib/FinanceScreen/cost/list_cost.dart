28406f903925be1b8e1d0b413b5551e78783894e
bwikimpeloles <exopink0394@gmail.com>
2023-01-23 12:49:45 +0800

    export csv

--- lib/FinanceScreen/cost/list_cost.dart
+++ lib/FinanceScreen/cost/list_cost.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:firestore_ui/animated_firestore_list.dart';
 import 'package:flutter/material.dart';
 import 'package:ftrsb_mobile/FinanceScreen/cost/add_cost.dart';
@@ -5,6 +8,10 @@ import 'package:ftrsb_mobile/FinanceScreen/cost/edit_cost.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ftrsb_mobile/FinanceScreen/cost/list_category.dart';
 import 'package:intl/intl.dart';
+import 'dart:io';
+import 'package:csv/csv.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:open_file/open_file.dart';
 
 class ListCostFinance extends StatefulWidget {
   @override
@@ -16,20 +23,80 @@ class _ListCostFinanceState extends State<ListCostFinance> {
   CollectionReference reference =
   FirebaseFirestore.instance.collection('Cost');
   var selectedCategory;
+  List<List<String>> listCost = [];
+  String? filePath;
+  List<List<dynamic>> _data = [];
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _ref = FirebaseFirestore.instance.collection('Cost')
         .orderBy('name');
+
   }
+
+  generateCsv() async{
+    String csvData = ListToCsvConverter().convert(listCost);
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('MM-dd-yyyy-HH-mm-ss').format(now);
+
+    Directory generalDownloadDir = Directory('/storage/emulated/0/Download');
+
+    final File file = await (File('${generalDownloadDir.path}/expenses_export_$formattedDate.csv').create());
+
+    await file.writeAsString(csvData).then((value) => OpenFile.open('${generalDownloadDir.path}/expenses_export_$formattedDate.csv'));
+
+    print(listCost.toString());
+  }
+
+  queryValues() async {
+    listCost.clear();
+    listCost = [<String>['Name', 'Category', 'Amount', 'Supplier', 'Date', 'Reference No. (Bank/PO/Invoice/Receipt)', 'Payment Type']];
+    CollectionReference _documentRef=FirebaseFirestore.instance.collection("Cost");
+    await _documentRef.get().then((ds){
+      if(ds!=null){
+        ds.docs.forEach((a){
+          final dynamic cost = a.data();
+          listCost.add([cost['name']??"", cost['category']??"", cost['amount']??"",cost['supplier']??"",DateFormat('dd/MM/yyyy').format((cost['date'] as Timestamp).toDate())??"", cost['referenceno']?? '', cost['paymenttype'] ?? '']);
+        });
+      } else{
+        print('fail');
+
+      }
+    });
+  }
+
+  // void _pickFile() async {
+  //
+  //   final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+  //
+  //   // if no file is picked
+  //   if (result == null) return;
+  //   // we will log the name, size and path of the
+  //   // first picked file (if multiple are selected)
+  //   print(result.files.first.name);
+  //   filePath = result.files.first.path!;
+  //
+  //   final input = File(filePath!).openRead();
+  //   final fields = await input
+  //       .transform(utf8.decoder)
+  //       .transform(const CsvToListConverter())
+  //       .toList();
+  //   print(fields);
+  //
+  //   setState(() {
+  //     _data = fields;
+  //   });
+  // }
+
 
   _showDeleteDialog({required Map cost}) {
     showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text('Delete ${cost['name']}'),
+            title: Text('Delete ${cost['name']??""}'),
             content: Text('Are you sure you want to delete?'),
             actions: [
               TextButton(
@@ -48,8 +115,9 @@ class _ListCostFinanceState extends State<ListCostFinance> {
   }
 
   Widget _buildCostItem({required Map cost}) {
+    //listCost.add([cost['name'], cost['category'], cost['amount'],cost['supplier']??"-",DateFormat('dd/MM/yyyy').format((cost['date'] as Timestamp).toDate()), cost['referenceno'], cost['paymenttype'] ?? '-']);
+    //'Name', 'Category', 'Amount', 'Supplier', 'Date', 'Reference No. (Bank/PO/Invoice/Receipt)', 'Payment Type'
     return GestureDetector(
-
       child: Padding(
         padding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
         child: Container(
@@ -84,7 +152,7 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                     ),
                     Flexible(
                       child: Text(
-                        cost['name'],
+                        cost['name']??"",
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w600),
@@ -105,7 +173,7 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                       width: 6,
                     ),
                     Text(
-                      cost['category'],
+                      cost['category']??"",
                       style: TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.w600),
@@ -128,7 +196,7 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                     ),
                     Flexible(
                       child: Text(
-                        cost['amount'],
+                        cost['amount']??"",
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w600),
@@ -176,7 +244,7 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                     ),
                     Flexible(
                       child: Text(
-                        DateFormat('dd/MM/yyyy').format((cost['date'] as Timestamp).toDate()),
+                          (cost['date']!=null)?DateFormat('dd/MM/yyyy').format((cost['date'] as Timestamp).toDate()):"",
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w600),
@@ -224,7 +292,7 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                     ),
                     Flexible(
                       child: Text(
-                        cost['referenceno'],
+                        cost['referenceno']??"",
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w600),
@@ -320,6 +388,14 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                     value: 0,
                     child: Text("Manage Category"),
                   ),
+                  PopupMenuItem<int>(
+                    value: 1,
+                    child: Text("Export CSV"),
+                  ),
+                  // PopupMenuItem<int>(
+                  //   value: 2,
+                  //   child: Text("Import CSV"),
+                  // ),
 
                 ];
               },
@@ -333,7 +409,20 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                     }),
                   ).then((value) => setState(() {}));
 
+                } else if(value == 1){
+                  print("Popup menu CSV");
+                  //await checkEmptyList();
+                  await queryValues();
+
+                  await generateCsv();
+                  Fluttertoast.showToast(msg: 'Expenses list was exported');
                 }
+                // else if(value == 2){
+                //   print("Import CSV");
+                //   //await checkEmptyList();
+                //   _pickFile();
+                //   Fluttertoast.showToast(msg: 'Expenses list was imported');
+                // }
               }
           ),
           ],
@@ -427,6 +516,7 @@ class _ListCostFinanceState extends State<ListCostFinance> {
                 itemBuilder: (BuildContext context, DocumentSnapshot? snapshot,
                     Animation<double> animation, int index) {
                   Map<String, dynamic> cost = snapshot?.data() as Map<String, dynamic>;
+                  //listCost.add([cost['name'], cost['category'], cost['amount'],cost['supplier']??"-",DateFormat('dd/MM/yyyy').format((cost['date'] as Timestamp).toDate()), cost['referenceno'], cost['paymenttype'] ?? '-']);
                   cost['key'] = snapshot?.id;
                   return _buildCostItem(cost: cost);
                 },
